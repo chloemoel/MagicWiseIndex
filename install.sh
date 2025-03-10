50fb4f115340b1fc07505fb2800afefc8a81390b
Chloe Moel <chloe-moel@uiowa.edu>
2025-03-10 11:49:53 -0500

    updating install script to add docker

--- install.sh
+++ install.sh
@@ -1,10 +1,21 @@
 #!/bin/bash
-#create singularity/apptainer containers with datasets and tools needed
-cd bin
 
-#vep
-singularity pull --name vep.sif docker://ensemblorg/ensembl-vep
-singularity exec vep.sif INSTALL.pl -c ../data -a cfp -s homo_sapiens -y GRCh37 --PLUGINS CADD
+# Check if the user passed "docker" as an argument
+if [ "$1" == "docker" ]; then
+    echo "Installing tools with Docker..."
 
-#python tools
-singularity build tools.sif tools.def
+    # Build Docker image for VEP
+    docker pull ensemblorg/ensembl-vep
+    docker run --rm -v "$(pwd)/data:/data" ensemblorg/ensembl-vep INSTALL.pl -c /data -a cfp -s homo_sapiens -y GRCh37 --PLUGINS CADD
+
+    # Build Docker image for Python tools (based on tools.def)
+    docker build -f tools.def -t tools .
+else
+    echo "Installing tools with Singularity..."
+    # Pull and execute the VEP container
+    singularity pull --name vep.sif docker://ensemblorg/ensembl-vep
+    singularity exec vep.sif INSTALL.pl -c ../data -a cfp -s homo_sapiens -y GRCh37 --PLUGINS CADD
+    
+    # Build Python tools container
+    singularity build tools.sif tools.def
+fi
