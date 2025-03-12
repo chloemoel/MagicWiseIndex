285a6430756d250090b5a7c7efc355fd7e4dc82a
Chloe Moel <chloe-moel@uiowa.edu>
2025-03-12 15:25:19 -0500

    more install edits

--- install.sh
+++ install.sh
@@ -8,11 +8,11 @@ if [ "$1" == "docker" ]; then
 
     # Build Docker image for VEP
     docker pull ensemblorg/ensembl-vep
-
-    docker run -t -i -v $home_dir/data/vep_data:/data ensemblorg/ensembl-vep INSTALL.pl -c $home_dir/data/vep_data -a cfp -s homo_sapiens -y GRCh37 --PLUGINS CADD
+    docker run -t -i -v $home_dir/data/vep_data:/data ensemblorg/ensembl-vep INSTALL.pl -a cfp -s homo_sapiens -y GRCh37 --PLUGINS CADD
 
     # Build Docker image for Python tools (based on tools.def)
     docker build -f tools_docker.def -t tools .
+    
 else
     echo "Installing tools with Singularity..."
     # Pull and execute the VEP container
