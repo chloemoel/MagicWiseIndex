3685801966bfc1f37ef174d83778cc26a279662d
Chloe Moel <chloe-moel@uiowa.edu>
2025-03-14 15:22:51 -0500

    updating install for CADD scripts

--- install.sh
+++ install.sh
@@ -8,7 +8,13 @@ if [ "$1" == "docker" ]; then
 
     # Build Docker image for VEP
     docker pull ensemblorg/ensembl-vep
-    docker run -t -i -v $home_dir/data/vep_data:/data ensemblorg/ensembl-vep INSTALL.pl -a cfp -s homo_sapiens -y GRCh37 --PLUGINS CADD
+    docker run -t -i -v $home_dir/data/vep_data:/data ensemblorg/ensembl-vep INSTALL.pl -a cfp -s homo_sapiens -y GRCh37
+
+    # install CADD files
+    mkdir -p ../data/vep_data/CADD
+    cd ../data/vep_data/CADD
+    wget -c https://krishna.gs.washington.edu/download/CADD/v1.7/GRCh37/whole_genome_SNVs.tsv.gz
+    wget -c https://krishna.gs.washington.edu/download/CADD/v1.7/GRCh37/whole_genome_SNVs.tsv.gz.tbi
 
     # Build Docker image for Python tools (based on tools.def)
     docker build -f tools_docker.def -t tools .
