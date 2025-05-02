5f95d990eb7bca659a29eb913c6fef96f7ef6cd1
Chloe Moel <chloe-moel@uiowa.edu>
2025-05-02 11:41:43 -0500

    updating install script

--- install.sh
+++ install.sh
@@ -1,6 +1,17 @@
 #!/bin/bash
-cd bin/data
+cd bin/containers
 
-# Download dbNSFP data
-#wget -c https://snpeff.blob.core.windows.net/databases/dbs/GRCh37/dbNSFP_4.1a/dbNSFP4.1a.txt.gz
-#wget -c https://snpeff.blob.core.windows.net/databases/dbs/GRCh37/dbNSFP_4.1a/dbNSFP4.1a.txt.gz.tbi
+
+# Check if the user passed "docker" as an argument
+if [ "$1" == "docker" ]; then
+    echo "Installing tools with Docker..."
+
+    # Build Docker image for Python tools (based on tools.def)
+    docker build -f tools_docker.def -t tools_docker .
+    
+else
+    echo "Installing tools with Singularity..."
+    
+    # Build Python tools container
+    singularity build tools.sif tools.def
+fi
