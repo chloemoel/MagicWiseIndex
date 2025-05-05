3744c800d59ebf18b816730947d964af835a25c5
Chloe Moel <chloe-moel@uiowa.edu>
2025-05-05 12:58:16 -0500

    updating install script

--- install.sh
+++ install.sh
@@ -6,10 +6,17 @@ if [ "$1" == "docker" ]; then
     echo "Installing tools with Docker..."
 
     # Build Docker image for Python tools (based on tools.def)
-    docker build -f tools_docker.def -t tools_docker .
+    docker build -f tools_docker.def -t tools_docker containers/
     
 else
     echo "Installing tools with Singularity..."
     # Build Python tools container
-    singularity build tools.sif tools.def
+    singularity build containers/tools.sif tools.def
 fi
+
+echo "Installing annotation info..."
+cd data/
+wget https://m-42bb44.6ba50.0ec8.data.globus.org/Shared/darbro-cytogenetics/Chloe/CombinedBurdenEstimate/bin/data/dbNSFP4.9a.MagicWise.txt.gz
+
+echo "Unzipping annotation info..."
+gunzip dbNSFP4.9a.MagicWise.txt.gz
