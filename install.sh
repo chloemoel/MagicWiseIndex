5d5dca918be7f57845f62547cba47a471c898129
Chloe Moel <chloe-moel@uiowa.edu>
2026-06-01 17:52:28 -0500

    update install script

--- install.sh
+++ install.sh
@@ -1,13 +1,11 @@
 #!/bin/bash
 cd bin/data
 
-wget https://m-42bb44.6ba50.0ec8.data.globus.org/Shared/darbro-cytogenetics/Chloe/CombinedBurdenEstimate/bin/data/dbNSFP4.9a.MagicWise.txt.gz
-
+wget https://github.com/chloemoel/MagicWiseIndex/releases/download/v.0.0.2/dbNSFP4.9a.MagicWise.txt.gz
 echo "Unzipping annotation info..."
 gunzip dbNSFP4.9a.MagicWise.txt.gz
 
-
-wget https://m-42bb44.6ba50.0ec8.data.globus.org/Shared/darbro-cytogenetics/Chloe/CombinedBurdenEstimate/bin/data/probe_info.csv.gz
+wget https://github.com/chloemoel/MagicWiseIndex/releases/download/v.0.0.2/probe_info.csv.gz
 echo "Unzipping methylation probe info..."
 gunzip probe_info.csv.gz
 
