4c222ba5db78728ee5820a43cbcdccb93559bf8e
Chloe Moel (Beck) <chloe-moel@uiowa.edu>
2026-08-13 13:19:22 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 cd bin/data
 
-wget https://github.com/chloemoel/MagicWiseIndex/releases/download/v.0.0.2/dbNSFP4.9a.MagicWise.txt.gz
+wget https://github.com/chloemoel/MagicWiseIndex/releases/download/v.0.0.3/dbNSFP4.9a.MagicWise.txt.gz
 echo "Unzipping annotation info..."
-tar -xvf dbNSFP4.9a.MagicWise.txt.gz
+gunzip dbNSFP4.9a.MagicWise.txt.gz
 
-wget https://github.com/chloemoel/MagicWiseIndex/releases/download/v.0.0.2/probe_info.csv.gz
+wget https://github.com/chloemoel/MagicWiseIndex/releases/download/v.0.0.3/probe_info.csv.gz
 echo "Unzipping methylation probe info..."
-tar -xvf probe_info.csv.gz
+gunzip probe_info.csv.gz
 
 echo "All done!"
